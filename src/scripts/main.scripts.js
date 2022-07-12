821a0690919c9a2d0c748d45b0f33f66c6482e89
Wigo Rossim da Silva <wigorossim@hotmail.com>
2022-07-12 18:44:06 -0300

    feat: adicionado o modal de edicao de perfil
    ainda sem a funcionalidade de edicao

--- src/scripts/main.scripts.js
+++ src/scripts/main.scripts.js
@@ -4,3 +4,5 @@ import headerController from "../controller/header.controller.js"
 
 //headerController.perfilFotoNome()
 headerController.dropDownMenu()
+headerController.abrirEditarPerfil()
+headerController.fecharEditarPerfil()
